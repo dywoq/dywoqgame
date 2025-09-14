d9a132bfd4ce04b5961c7694532a9a8765c810e9
dywoq <aleks.koyf@gmail.com>
2025-09-14 17:11:24 +0300

    [api/1.0.0] write Console module

--- api/src/lib.dywoqgame.1.0.0.d.ts
+++ api/src/lib.dywoqgame.1.0.0.d.ts
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//#region "error handling"
 export declare interface Error {
 	/** Returns the error message. Empty string means there's no error. */
 	message(): string
@@ -20,3 +21,68 @@ export declare interface Error {
 }
 /** Creates and returns new Error instance, with message.  */
 export function createError(message?: string): Error
+//#endregion
+
+//#region "console"
+export type ConsoleStreamType = "log" | "error" | "fatal" | "warning" | "debug"
+
+export declare interface ConsoleStreamWriter {
+	/** 
+	 * Writes the data to the stream.
+	 * May return a error.
+	*/
+	write(...data: any[]): Error
+}
+
+export declare interface ConsoleStreamReader {
+	/** 
+	 * Returns the data of stream.
+	 * May return a error.
+	 */
+	read(): { got: any[], error: Error }
+}
+
+export declare interface ConsoleStreamFlusher {
+	/** 
+	 * Flushes and returns the data of stream.
+	 * May return a error.
+	 */
+	flush(): { flushed: any[], error: Error }
+}
+
+export declare interface ConsoleStream extends ConsoleStreamFlusher, ConsoleStreamReader, ConsoleStreamWriter { }
+
+export declare interface ConsoleStreams {
+	/** Stream of logs. */
+	log: ConsoleStream
+	/** Stream of errors. */
+	error: ConsoleStream
+	/** Stream of fatal errors. */
+	fatal: ConsoleStream
+	/** Stream of warnings. */
+	warning: ConsoleStream
+	/** Stream of debugging messages. */
+	debug: ConsoleStream
+}
+
+export declare interface ConsoleStreamsWriting {
+	/**
+	 * Writes the arguments to the chosen stream.
+	 * May return a error.
+	 * @param stream The chosen stream.
+	 * @param args Arguments to write.
+	 */
+	write(stream: ConsoleStreamType, ...args?: any[]): Error
+	/**
+	 * Writes the arguments to the chosen stream with new line.
+	 * May return a error.
+	 * @param stream The chosen stream.
+	 * @param args Arguments to write.
+	 */
+	writeln(stream: ConsoleStreamType, ...args?: any[]): Error
+}
+
+export declare let ConsoleStreams: ConsoleStreams
+export declare let ConsoleStreamsWriting: ConsoleStreamsWriting
+
+//#endregion
