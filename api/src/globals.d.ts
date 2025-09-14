117ca35591cf73bef634bbb77f798d5f20731527
dywoq <aleks.koyf@gmail.com>
2025-09-14 10:31:48 +0300

    [api] add globals.d.ts

---
+++ api/src/globals.d.ts
@@ -0,0 +1,19 @@
+declare type string = any;
+
+declare type number = any;
+
+declare type boolean = any;
+
+declare type undefined = any;
+
+declare class Object { }
+declare class Function { }
+declare class Boolean { }
+declare class Number { }
+declare class String { }
+declare class Array<T> { }
+declare class RegExp { }
+declare class Symbol { }
+declare class Error { }
+
+declare interface IArguments { }
