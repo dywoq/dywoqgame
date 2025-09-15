4ca88ebf5048e983b276735d32beba1d17812e9f
dywoq <aleks.koyf@gmail.com>
2025-09-15 18:28:51 +0300

    [tools/dywoqgames/internal/config] add more fields to Config

--- tools/dywoqgamec/internal/config/config.go
+++ tools/dywoqgamec/internal/config/config.go
@@ -6,7 +6,12 @@ import (
 )
 
 type Config struct {
-	Title string `json:"title"`
+	Title   string   `json:"title"`
+	Version int      `json:"version"`
+	Owner   string   `json:"owner"`
+	Link    string   `json:"link"`
+	Year    int      `json:"year"`
+	Authors []string `json:"authors"`
 }
 
 // Fill fills the config instance with the information from the Json config, which located at path.
