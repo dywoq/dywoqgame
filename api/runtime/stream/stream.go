e3fe30077e7efe56e77aa0267cb3db733cd97c33
dywoq <aleks.koyf@gmail.com>
2025-09-16 09:10:48 +0300

    [api/runtime] add stream package

---
+++ api/runtime/stream/stream.go
@@ -0,0 +1,37 @@
+package stream
+
+// Type represents stream type.
+type Type string
+
+const (
+	Error   Type = "error"
+	Warning Type = "warning"
+	Fatal   Type = "fatal"
+	Out     Type = "out"
+	Debug   Type = "debug"
+)
+
+// Write writes p to the t stream, returning amount of allocations.
+// May return a error if t is wrong type.
+//
+// If stream data is out of maximum length (100), it removes the last message from the stream
+// for your new message.
+func Write(t Type, p []byte) (int, error) { return 0, nil } // stub
+
+// Flush flushes the t stream and returns its data.
+// May return a error if t is wrong type.
+func Flush(t Type) ([]byte, error) { return []byte{}, nil } // stub
+
+// Clean cleans the t stream without returning it.
+// May return a error if t is wrong type.
+func Clean(t Type) error { return nil } // stub
+
+// Get gets the data from t stream.
+// May return a error if t is wrong type
+func Get(t Type) ([]byte, error) { return []byte{}, nil } // stub
+
+// Capacity returns the capacity of the t stream data.
+func Capacity(t Type) int { return 0 }
+
+// Length returns the length of the t stream data.
+func Length(t Type) int { return 0 }
