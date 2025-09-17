5c6bb0286aa9e011dc7fa980d9aa1410a92fa02f
dywoq <aleks.koyf@gmail.com>
2025-09-17 21:27:24 +0300

    [tools/dglc/context] add Peek and more methods to Context interface

--- tools/dglc/parser/context/context.go
+++ tools/dglc/parser/context/context.go
@@ -1,6 +1,10 @@
 package context
 
-import "github.com/dywoq/dywoqgame/tools/dglc/parser/expression"
+import (
+	"go/token"
+
+	"github.com/dywoq/dywoqgame/tools/dglc/parser/expression"
+)
 
 type Converters interface {
 	String(expr *expression.Expression) (string, bool)
@@ -14,6 +18,25 @@ type Converters interface {
 	UInt64(expr *expression.Expression) (uint64, bool)
 }
 
+type Peek interface {
+	// Current returns the current token that is processed by parser.
+	// It can return nil if there's no processing tokens.
+	Current() *token.Token
+
+	// Past returns the past expression that was already processed.
+	// It can return nil if there's no processed expressions.
+	Past() *expression.Expression
+
+	// Next returns the next token.
+	// It can return nil if the parser reached the end.
+	Next() *token.Token
+}
+
 type Context interface {
 	Converters
+	Peek
+
+	// Advance advances the parser to the next token, skipping the current token.
+	// Returns false if there were no left tokens.
+	Advance() bool
 }
