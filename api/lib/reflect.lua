8afce467fb219782886837f404df4ef4627ee528
dywoq <aleks.koyf@gmail.com>
2025-09-19 18:09:38 +0300

    [api] add reflect module

---
+++ api/lib/reflect.lua
@@ -0,0 +1,11 @@
+reflect = {}
+
+--- Returns the type of a.
+--- @param a any The parameter to get type from.
+--- @return string
+function reflect.type(a) end
+
+-- Returns a table containing all methods (functions) of the given table.
+-- @param table table The table to get methods from.
+-- @return table A table of function names.
+function reflect.get_methods(table) end
