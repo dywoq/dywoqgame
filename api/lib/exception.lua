14e4ca7bddfd3fb2e08d123410f3845030fcc54b
dywoq <aleks.koyf@gmail.com>
2025-09-19 16:11:04 +0300

    [api/lib] add exception module

---
+++ api/lib/exception.lua
@@ -0,0 +1,24 @@
+exception = {}
+
+--- Throws an exception with the given message.
+--- If an exception is already in the thrown state, this function throws
+--- a new exception to indicate a nested error.
+--- @param message any The error message to be set.
+function exception.throw(message) end
+
+--- Reports whether an exception has been thrown.
+--- @return boolean
+function exception.thrown() end
+
+--- Gets the message of the thrown exception and clears the state.
+--- Use this function to catch and handle an exception.
+--- @return any The exception message, or nil if no exception was thrown.
+function exception.get() end
+
+--- Terminates the game immediately without cleaning up entities.
+--- This is for fatal, unrecoverable errors.
+function exception.terminate() end
+
+--- exception.get_chain_messages returns a table of all nested error messages.
+--- @return table A table containing all messages in the exception chain.
+function exception.get_chain_messages() end
