3dad32737807508a3d05121d0b5ece6be6cb2aa4
dywoq <aleks.koyf@gmail.com>
2025-09-24 21:56:15 +0300

    [Logging.Stream] Add Dispose method in Stream class

--- DywoqGame/Logging/Stream/Stream.cs
+++ DywoqGame/Logging/Stream/Stream.cs
@@ -1,11 +1,13 @@
 namespace DywoqGame.Logging.Stream;
 
-public class Stream(byte maxSize) : IStreamFlusher, IStreamSize, IStreamWriter, IStreamGet
+public class Stream(byte maxSize) : IStreamFlusher, IStreamSize, IStreamWriter, IStreamGet, IDisposable
 {
-	public List<object> buffer = [];
+	private List<object> buffer = [];
+	private bool disposed = false;
 
 	public object[]? Flush()
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		var flushed = buffer;
 		buffer.Clear();
 		return flushed?.ToArray();
@@ -13,26 +15,27 @@ public class Stream(byte maxSize) : IStreamFlusher, IStreamSize, IStreamWriter, 
 
 	public List<object>? GetBuffer()
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		return buffer ?? [];
 	}
 
 	public object? GetLastMessage()
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		return buffer.Count == 0 ? null : buffer[^1];
 	}
 
-	public byte MaxSize()
-	{
-		return maxSize;
-	}
+	public byte MaxSize() => maxSize;
 
 	public int Size()
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		return (byte)buffer.Count;
 	}
 
 	public void Write(object message)
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		if (buffer.Count >= maxSize)
 		{
 			buffer.RemoveAt(0);
@@ -42,12 +45,33 @@ public class Stream(byte maxSize) : IStreamFlusher, IStreamSize, IStreamWriter, 
 
 	public override bool Equals(object? obj)
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		return obj is Stream s && Size() == s.Size();
 	}
 
 	public override int GetHashCode()
 	{
+		ObjectDisposedException.ThrowIf(disposed, this);
 		return Size().GetHashCode();
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposing)
+		{
+			if (disposed)
+			{
+				buffer.Clear();
+				buffer = null!;
+			}
+			disposed = true;
+		}
+	}
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
 	}
 
 	public static Stream operator +(Stream s, object message)
