1dea3da3d241c951d5e076b305908b103769fad6
dywoq <aleks.koyf@gmail.com>
2025-09-30 18:45:46 +0300

    feature(graphics): add Position and Size

---
+++ graphics/position.go
@@ -0,0 +1,6 @@
+package graphics
+
+type Position struct {
+	X float32 `json:"x"`
+	Y float32 `json:"y"`
+}
---
+++ graphics/size.go
@@ -0,0 +1,6 @@
+package graphics
+
+type Size struct {
+	Width  float32 `json:"width"`
+	Height float32 `json:"height"`
+}
